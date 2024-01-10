d5ac683211d207679577fc125ae4dabfb7bba5ab
Josh Fabean <me@joshfabean.com>
2024-01-10 10:22:15 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# FabeanOS 🟰 ❄️ NixOS Configs
+
+FabeanOS is my production machines config files.
+
+As of right now this is basically a copy of [ZaneyOS](https://gitlab.com/Zaney/zaneyos) with just some changes to get packages I need working.
+
+![](demo.jpg)
+
+## What Is NixOS
+
+NixOS is a Linux distribution known for its unique approach to package management and system configuration. It uses the Nix package manager, which is based on a purely functional approach to managing software and system configurations.
+
+In NixOS, the entire operating system configuration is described declaratively in a configuration file, allowing for reproducibility and easy rollbacks. This means that changes to the system can be tracked and reversed, making it robust and reliable for system administrators and developers.
+
+## Why Choose NixOS
+
+Choosing NixOS often depends on specific needs and preferences. Here are some reasons why people opt for NixOS:
+
+### Declarative Configuration
+
+NixOS employs a declarative approach to system configuration. This means the entire system configuration is defined in a single file, making it reproducible and easier to manage. Changes are tracked and can be rolled back if needed.
+
+### Functional Package Management
+
+The Nix package manager ensures that each package and its dependencies are isolated and managed separately. This prevents conflicts between different versions of software and enables easy rollbacks to previous versions.
+
+### Reproducibility
+
+NixOS allows for consistent and reproducible environments, critical in development, testing, and deployment scenarios. It's particularly valuable in DevOps and CI/CD pipelines where consistent environments are essential.
+
+### Multiple Environments and Rollbacks 
+
+With NixOS, it's possible to have multiple versions of the same software installed simultaneously without conflicts. This flexibility is especially useful for developers or those requiring different versions of software for various projects.
+
+### Community and Customization
+
+NixOS has an active community that contributes to its growth, sharing configurations, and offering support. Its customizable nature allows users to tailor their systems to their needs.
+
+# Steps To Reproduce My System
+
+
+Clone this repo, replace your hardware-configuration.nix with the one inside the workstation folder, enable flakes in your default configuration.nix, then go into repo folder and run this command:
+
+```
+sudo nixos-rebuild switch --flake .#farfetchd
+```
+
+Hope you enjoy!
---
+++ farfetchd/configuration.nix
@@ -0,0 +1,156 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  networking.hostName = "farfetchd"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.josh = {
+    isNormalUser = true;
+    description = "Josh Fabean";
+    extraGroups = [ "networkmanager" "wheel" "disk" "power" "video" "docker"];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = pkgs.lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
+    };
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim wget curl
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+  services.blueman.enable = true;
+  services.hardware.bolt.enable = true;
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+  services.openssh.enable = true;
+  services.fstrim.enable = true;
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "";
+    libinput.enable = true;
+    displayManager.sddm.enable = true;
+  };
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  services.syncthing = {
+    enable = true;
+    user = "josh";
+    dataDir = "/home/josh/Sync";
+    configDir = "/home/josh/Sync/.config/syncthing";
+  };
+  services.tailscale = {
+   enable = true;
+   useRoutingFeatures = "both";
+  };
+  hardware.pulseaudio.enable = false;
+  sound.enable = true;
+  security.rtkit.enable = true;
+
+
+  system.stateVersion = "23.11";
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  ## Docker & DDEV
+  virtualisation.docker.enable = true;
+  ## 9003 - xdebug
+  ## 8384, 22000 Syncthing
+  networking.firewall.allowedTCPPorts = [ 8384 22000 9003 ];
+  ## Syncthing
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+  environment.etc.hosts.mode = "0644";
+}
