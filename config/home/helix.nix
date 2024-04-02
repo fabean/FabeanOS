c0d45b67959dd4b0d8816bee35a3ba60a58c4d2b
Josh Fabean <me@joshfabean.com>
2024-04-01 22:13:24 -0400

    adding more stuff to helix

--- config/home/helix.nix
+++ config/home/helix.nix
@@ -17,25 +17,40 @@ in {
         command = "${typescript-language-server}/bin/typescript-language-server";
         args = [ "--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib" ];
       };
-      language = [{
-        name = "php";
-        file-types = [
-          "php"
-          "inc"
-          "php4"
-          "php5"
-          "phtml"
-          "ctp"
-          "module"
-          "theme"
-        ];
-      }];
+      language = [
+        {
+          name = "php";
+          file-types = [
+            "php"
+            "inc"
+            "php4"
+            "php5"
+            "phtml"
+            "ctp"
+            "module"
+            "theme"
+          ];
+          debugger = {
+            name = "vscode-php-debug";
+            transport = "stdio";
+            command = "node";
+            args = [ "/home/josh/.vscode/extensions/xdebug.php-debug-1.34.0/out/phpDebug.js" ];
+            templates = [{
+              name = "Listen for XDebug";
+              request = "launch";
+              completion = ["ignored"];
+              args = {};
+            }];
+          };
+        }
+      ];
     };
 
     settings = {
       keys.normal = {
         space.space = "file_picker";
       };
+      editor.file-picker.hidden = false;
     };
 
   };
