7f9aacb8365684f63db5248b7b8183e4800bcadd
Josh Fabean <me@joshfabean.com>
2024-11-23 10:57:25 -0500

    remove drop_shadow from hyprland conf

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -114,7 +114,6 @@ with lib;
           }
           decoration {
             rounding = 10
-            drop_shadow = false
             blur {
                 enabled = true
                 size = 5
