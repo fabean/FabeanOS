e5615cfbee0c22ee84e8b7890fb9d2ab03a0d274
Josh Fabean <me@joshfabean.com>
2025-05-08 11:16:23 -0400

    move ddev to unstable

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix/release-24.11";
@@ -26,7 +27,7 @@
     # }
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, flatpaks, nixos-cosmic, ... }:
+  outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, flatpaks, nixos-cosmic, ... }:
     let
       system = "x86_64-linux";
       host = "farfetchd";
